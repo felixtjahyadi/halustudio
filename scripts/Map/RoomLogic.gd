031820cfca7a3fdde808b5e8632262388a8d6b60
qdave5 <qosimd@gmail.com>
2024-04-24 11:24:00 +0700

    feat: add option to show portal or not in room

--- scripts/Map/RoomLogic.gd
+++ scripts/Map/RoomLogic.gd
@@ -23,12 +23,20 @@ const MAP_TEXTURES : Dictionary = {
 
 @export var skippable : bool = false
 
+@export var lastRoom : bool = false
+@export var levelPath : String = "Level 1"
+
 @onready var roomTileMap : TileMap = get_node("RoomTileMap")
+
+var portalScenePath : String = "res://scenes/Map/Objects/Portal.tscn"
 
 func _ready():
 	_isRoomNotExplored = false if skippable else true
 	_updateRoomTexture()
 	toggleDoor()
+	
+	if skippable and lastRoom:
+		showPortal()
 
 func _updateRoomTexture():
 	roomTileMap.tile_set.get_source(0).texture = load(MAP_TEXTURES[mapTexture].wall)
@@ -46,6 +54,11 @@ func toggleDoor():
 	roomTileMap.set_layer_enabled(1, _isDoorClosed)
 	_toggleIsDoorClosed()
 
+func showPortal():
+	var portal = load(portalScenePath).instantiate()
+	portal.levelPath = levelPath
+	add_child(portal)
+
 func startRoom():
 	if _isRoomNotExplored:
 		_isRoomNotExplored = false
@@ -55,6 +68,8 @@ func startRoom():
 func endRoom():
 	await get_tree().create_timer(1.0).timeout
 	toggleDoor()
+	if lastRoom:
+		showPortal()
 
 func _on_room_area_body_entered(body):
 	if body.get_name() == "Player":
