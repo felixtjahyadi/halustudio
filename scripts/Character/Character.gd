acf039076b5238f97128e1f12eb92e5e338c4a44
Lyzander Marciano Andrylie <lyzanderandrylie@gmail.com>
2024-04-24 14:25:20 +0700

    feat: add class hierarchy

--- scripts/Character/Character.gd
+++ scripts/Character/Character.gd
@@ -2,8 +2,8 @@ extends CharacterBody2D
 
 class_name Character
 
-@export var weapon_scene: PackedScene
 @export var speed = 300.0
+@export var weapon_scene: PackedScene
 
 var current_weapon: Node
 
@@ -20,3 +20,27 @@ func move():
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed if direction else Vector2.ZERO
 	move_and_slide()
+
+# Character Basic Animation
+@onready var animations = $AnimationTree
+
+var last_direction = Vector2(0.1, 0.1)
+
+func _process(delta):
+	set_animation()
+
+func set_animation():
+	set_walking()
+	set_blend_position()
+
+func set_walking():
+	var direction = Input.get_vector("left", "right", "up", "down")
+	var is_walking = direction != Vector2.ZERO
+	animations["parameters/conditions/is_walking"] = is_walking
+	animations["parameters/conditions/idle"] = not is_walking
+	
+	if is_walking and direction.x != 0: last_direction = direction
+
+func set_blend_position():
+	animations["parameters/Idle/blend_position"] = last_direction.x
+	animations["parameters/Walk/blend_position"] = last_direction.x
