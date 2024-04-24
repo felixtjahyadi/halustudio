b3e3f372b77a6ba6fd9601534975299df5381595
qdave5 <qosimd@gmail.com>
2024-04-24 17:59:14 +0700

    add spawner (need improvement)

--- scripts/Enemy/Boss/Boss.gd
+++ scripts/Enemy/Boss/Boss.gd
@@ -2,8 +2,15 @@ extends Enemy
 
 class_name Boss
 
+var isAwake : bool = false
+
+func _process(delta):
+	if isAwake:
+		await get_tree().create_timer(attack_cooldown).timeout
+		attack()
+
 func start_animation():
-	pass
+	isAwake = true
 
 func end_animation():
 	pass
--- scripts/Enemy/Boss/CorruptedTreeGuardian.gd
+++ scripts/Enemy/Boss/CorruptedTreeGuardian.gd
@@ -3,17 +3,26 @@ extends Boss
 class_name CorruptedTreeGuardian
 
 @export var maxMinionCount : int = 15
-var minionList : Array = []
+@export var minionList : Array = [preload("res://scenes/Enemy/Boss/BranchMinion.tscn")]
+@export var intervalSpawn : float = 1.0
 
 @onready var sprite : AnimatedSprite2D = $AnimatedSprite2D
+
+var onFieldMinions = []
 
 func _ready():
 	#sprite.play("idle")
 	sprite.play("sleep")
-	pass
+	super()
+
+func _process(delta):
+	if isAwake and onFieldMinions.size() == 0:
+		var count = _get_minion_count_randomizer()
+		_spawn_minion(count)
+	super(delta)
 
 func attack():
-	if (minionList.size() <= maxMinionCount):
+	if (onFieldMinions.size() <= maxMinionCount):
 		var count = _get_minion_count_randomizer()
 		_spawn_minion(count)
 
@@ -21,10 +30,26 @@ func _get_minion_count_randomizer():
 	return randi_range(2, 5)
 
 func _spawn_minion(count = 1):
-	pass
+	for i in count:
+		_spawn()
+		await get_tree().create_timer(intervalSpawn).timeout
+
+func _spawn():
+	var selectedMinion = minionList.pick_random()
+	var minion = selectedMinion.instantiate()
+	get_parent().add_child(minion)
+	
+	var spawn_pos = Vector2(0, 0)
+	spawn_pos.x = randf_range(-100, 100)
+	spawn_pos.y = randf_range(-100, 100)
+	
+	minion.position = spawn_pos
+	
+	onFieldMinions.append(minion)
 
 func start_animation():
 	await get_tree().create_timer(3.0).timeout
 	sprite.play("awake")
 	await get_tree().create_timer(2.0).timeout
 	sprite.play("idle")
+	super()
