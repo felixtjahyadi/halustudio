77d982e50a28f53995ab6b8cb5341f1e22a28381
qdave5 <qosimd@gmail.com>
2024-04-24 23:40:55 +0700

    navigate mainmenu to Level 1

--- scripts/title.gd
+++ scripts/title.gd
@@ -1,6 +1,6 @@
 extends Control
 
-var level = "res://scenes/world.tscn"
+var level = "res://scenes/Map/Level/Level 1.tscn"
 
 func _on_play_click_end():
 	var _level = get_tree().change_scene_to_file(level)
