bef277838af1f37d021c1764ef2eb75d95a59823
Felix Tjahyadi <felix.tjahyadi@ui.ac.id>
2024-04-24 18:38:47 +0700

    Melee implemented

--- scripts/Character/Concrete/Wade.gd
+++ scripts/Character/Concrete/Wade.gd
@@ -1,1 +1,11 @@
 extends MeleeCharacter
+
+@onready var weapon: Node2D = get_node("Sword")
+
+func _process(delta):
+	var mouse_direction: Vector2 = (get_global_mouse_position() - global_position).normalized()
+	weapon.rotation = mouse_direction.angle()
+	if weapon.scale.y == 2 and mouse_direction.x<0:
+		weapon.scale.y = -2
+	elif weapon.scale.y == -2 and mouse_direction.x>0:
+		weapon.scale.y = 2
